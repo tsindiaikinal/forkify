871cfdb26d5741ef9c37f19a9b3f343421367149
tsindiaikinal <1914.ac@gmail.com>
2023-05-08 16:16:29 +0300

    [model] added new file - MVC pattern. Refactoring

---
+++ src/js/model.js
@@ -0,0 +1,30 @@
+import { API_URL } from "./config";
+import { getJSON } from "./helpers";
+
+export const state = {
+  recipe: {},
+};
+
+export const loadRecipe = async function (id) {
+  try {
+  const data = await getJSON(`${API_URL}/${id}`);
+
+    const { recipe } = data.data;
+
+    state.recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients,
+    };
+
+    // console.log(state.recipe);
+  } catch (error) {
+    alert(error);
+    console.log(error);
+  }
+};
