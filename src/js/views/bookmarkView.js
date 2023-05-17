b9bd24aabe32698152e13b2e76a6e6ca4796086b
Oleksii Tsyndiaikin <1914.ac@gmail.com>
2023-05-17 15:09:07 +0300

    [bookmarkView] new file. New feature - adding a bookmark for a favorite recipe

---
+++ src/js/views/bookmarkView.js
@@ -0,0 +1,11 @@
+import View from './View';
+import icons from '../../img/icons.svg';
+
+class BookmarkView extends View {
+
+  constructor() {
+    super();
+  }
+}
+
+export default new BookmarkView();
