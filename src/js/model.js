fbd0622304e95f698eca0a192c41392764f3ca2b
Oleksii Tsyndiaikin <1914.ac@gmail.com>
2023-05-17 15:20:07 +0300

    [js] added new business logic for new features

--- src/js/model.js
+++ src/js/model.js
@@ -1,13 +1,20 @@
-import { API_URL } from "./config";
-import { getJSON } from "./helpers";
+import { API_URL, ITEMS_PER_PAGE } from './config';
+import { getJSON } from './helpers';
 
 export const state = {
   recipe: {},
+  search: {
+    query: '',
+    result: [],
+    itemsPerPage: ITEMS_PER_PAGE,
+    page: 1,
+  },
+  bookmarks: [],
 };
 
 export const loadRecipe = async function (id) {
   try {
-  const data = await getJSON(`${API_URL}/${id}`);
+    const data = await getJSON(`${API_URL}/${id}`);
 
     const { recipe } = data.data;
 
@@ -22,9 +29,71 @@ export const loadRecipe = async function (id) {
       ingredients: recipe.ingredients,
     };
 
+    if (state.bookmarks.some(bookmark => bookmark.id === id)) {
+      state.recipe.bookmarked = true;
+    } else {
+    state.recipe.bookmarked = false;
+    }
     // console.log(state.recipe);
   } catch (error) {
-    alert(error);
-    console.log(error);
+    console.log(`${error} 💥 💥 💥`);
+    throw error;
   }
 };
+
+export const loadSearchResults = async function (query) {
+  try {
+    state.search.query = query;
+
+    const data = await getJSON(`${API_URL}?search=${query}`);
+
+    state.search.result = data.data.recipes.map(recipe => {
+      return {
+        id: recipe.id,
+        title: recipe.title,
+        image: recipe.image_url,
+        publisher: recipe.publisher,
+      };
+    });
+    // console.log(state.search.result);
+  } catch (error) {
+    console.log(`${error} 💥 💥 💥`);
+    throw error;
+  }
+};
+
+export const getSearchResultPage = function (page = state.search.page) {
+  state.search.page = page;
+
+  const start = (page - 1) * state.search.itemsPerPage;
+  const end = start + state.search.itemsPerPage;
+
+  return state.search.result.slice(start, end);
+};
+
+export const updateServings = function (newServings) {
+  state.recipe.ingredients.forEach(ingr => {
+    ingr.quantity = (ingr.quantity * newServings) / state.recipe.servings;
+  });
+  state.recipe.servings = newServings;
+};
+
+export const addBookmark = function (recipe) {
+  // Add bookmark
+  state.bookmarks.push(recipe);
+
+  // Mark current recipe as bookmark
+  if (recipe.id === state.recipe.id) state.recipe.bookmarked = true;
+  // console.log(state.recipe)
+};
+
+export const deleteBookmark = function (id) {
+  // Get bookmark index
+  const index = state.bookmarks.findIndex(el => el.id === id);
+
+  // Delete selected bookmark
+  state.bookmarks.splice(index, 1);
+
+  // Unmark current recipe as bookmark
+  if (recipe.id === state.recipe.id) state.recipe.bookmarked = false;
+}
