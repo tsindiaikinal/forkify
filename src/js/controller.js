4eafaa10c14e498ca9739ea27b687aa10aad2ad6
Oleksii Tsyndiaikin <1914.ac@gmail.com>
2023-05-17 15:17:22 +0300

    [js] added new controllers for new features. Refactoring...

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,169 +1,95 @@
-('use strict');
-import icons from '../img/icons.svg';
+import * as model from './model';
+import bookmarkView from './views/bookmarkView';
+import paginationView from './views/paginationView';
+import recipeView from './views/recipeView';
+import resultView from './views/resultView';
+import searchView from './views/searchView';
 
-console.log(icons);
-
-const recipeContainer = document.querySelector('.recipe');
-
-const timeout = function (s) {
-  return new Promise(function (_, reject) {
-    setTimeout(function () {
-      reject(new Error(`Request took too long! Timeout after ${s} second`));
-    }, s * 1000);
-  });
-};
-
-// https://forkify-api.herokuapp.com/v2
+// For Parcel bilder
+/* if (module.hot) {
+  module.hot.accept();
+} */
 
 ///////////////////////////////////////
 
-// Render spinner function
-const renderSpinner = function (parentEl) {
-  const tmpl = `
-      <div class="spinner">
-        <svg>
-          <use href="${icons}#icon-loader"></use>
-        </svg>
-      </div>
-  `;
-  parentEl.innerHTML = '';
-  parentEl.insertAdjacentHTML('afterbegin', tmpl);
-};
-
-const showRecipe = async function () {
+const controlRecipes = async function () {
   try {
     const id = window.location.hash.slice(1);
     if (!id) return;
-    
+
+    // Render spinner function
+    recipeView.renderSpinner();
+
+    // Update result view to mark selected search result
+    resultView.render(model.getSearchResultPage());
+
     // Loading recipe
-    renderSpinner(recipeContainer);
-    
-    const res = await fetch(
-      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
-    );
-    const data = await res.json();
-
-    if (!res.ok) throw new Error(`${data.message} Error code (${res.status})`);
-    // console.log(data.data);
-    let { recipe } = data.data;
-
-    recipe = {
-      id: recipe.id,
-      title: recipe.title,
-      publisher: recipe.publisher,
-      sourceUrl: recipe.source_url,
-      image: recipe.image_url,
-      servings: recipe.servings,
-      cookingTime: recipe.cooking_time,
-      ingredients: recipe.ingredients,
-    };
-    console.log(recipe);
+    await model.loadRecipe(id);
 
     // Rendering recipe
-
-    const tmpl = `
-        <figure class="recipe__fig">
-          <img src="${recipe.image}" alt="${
-      recipe.title
-    }" class="recipe__img" />
-          <h1 class="recipe__title">
-            <span>${recipe.title}</span>
-          </h1>
-        </figure>
-
-        <div class="recipe__details">
-          <div class="recipe__info">
-            <svg class="recipe__info-icon">
-              <use href="${icons}#icon-clock"></use>
-            </svg>
-            <span class="recipe__info-data recipe__info-data--minutes">${
-              recipe.cookingTime
-            }</span>
-            <span class="recipe__info-text">minutes</span>
-          </div>
-          <div class="recipe__info">
-            <svg class="recipe__info-icon">
-              <use href="${icons}#icon-users"></use>
-            </svg>
-            <span class="recipe__info-data recipe__info-data--people">${
-              recipe.servings
-            }</span>
-            <span class="recipe__info-text">servings</span>
-
-            <div class="recipe__info-buttons">
-              <button class="btn--tiny btn--increase-servings">
-                <svg>
-                  <use href="${icons}#icon-minus-circle"></use>
-                </svg>
-              </button>
-              <button class="btn--tiny btn--increase-servings">
-                <svg>
-                  <use href="${icons}#icon-plus-circle"></use>
-                </svg>
-              </button>
-            </div>
-          </div>
-
-          <div class="recipe__user-generated">
-            <svg>
-              <use href="${icons}#icon-user"></use>
-            </svg>
-          </div>
-          <button class="btn--round">
-            <svg class="">
-              <use href="${icons}#icon-bookmark-fill"></use>
-            </svg>
-          </button>
-        </div>
-
-        <div class="recipe__ingredients">
-          <h2 class="heading--2">Recipe ingredients</h2>
-          <ul class="recipe__ingredient-list">
-          ${recipe.ingredients
-            .map(ingr => {
-              return `
-          <li class="recipe__ingredient">
-            <svg class="recipe__icon">
-              <use href="${icons}#icon-check"></use>
-            </svg>
-            <div class="recipe__quantity">${ingr.quantity}</div>
-            <div class="recipe__description">
-              <span class="recipe__unit">${ingr.unit}</span>
-              ${ingr.description}
-            </div>
-          </li>`;
-            })
-            .join('')}
-          </ul>
-        </div>
-
-        <div class="recipe__directions">
-          <h2 class="heading--2">How to cook it</h2>
-          <p class="recipe__directions-text">
-            This recipe was carefully designed and tested by
-            <span class="recipe__publisher">${
-              recipe.publisher
-            }</span>. Please check out
-            directions at their website.
-          </p>
-          <a
-            class="btn--small recipe__btn"
-            href="${recipe.sourceUrl}"
-            target="_blank"
-          >
-            <span>Directions</span>
-            <svg class="search__icon">
-              <use href="${icons}#icon-arrow-right"></use>
-            </svg>
-          </a>
-        </div>`;
-    recipeContainer.innerHTML = '';
-    recipeContainer.insertAdjacentHTML('afterbegin', tmpl);
-  } catch (error) {
-    console.log(error);
-  } finally {
+    recipeView.render(model.state.recipe);
+  } catch (err) {
+    recipeView.renderError();
   }
 };
 
-// showRecipe();
-['hashchange', 'load'].forEach(ev => window.addEventListener(ev, showRecipe));
+const controlSearchResult = async function () {
+  try {
+    // Get search query
+    const query = searchView.getQuery();
+    if (!query) return;
+
+    // Render spinner function
+    resultView.renderSpinner();
+
+    // Load search result
+    await model.loadSearchResults(query);
+
+    // Render search result
+    // resultView.render(model.state.search.result);
+    resultView.render(model.getSearchResultPage(1));
+
+    // Render initial pagination buttons
+    paginationView.render(model.state.search);
+  } catch (error) {
+    console.log(error);
+    resultView.renderError(error);
+  }
+};
+
+const controlPagination = function (goToPage) {
+  try {
+    // Render new result
+    resultView.render(model.getSearchResultPage(goToPage));
+
+    // Render new pagination buttons
+    paginationView.render(model.state.search);
+  } catch (error) {
+    console.log(error);
+    paginationView.renderError(error);
+  }
+};
+
+const controlServings = function (newSevings) {
+  // Update the recipe servings (in state)
+  model.updateServings(newSevings);
+
+  // Update the recipe view
+  // recipeView.render(model.state.recipe);
+  recipeView.update(model.state.recipe);
+};
+
+const controlAddBookmark = function () {
+  model.addBookmark(model.state.recipe);
+  
+  recipeView.update(model.state.recipe);
+};
+
+const init = function () {
+  recipeView.addHandlerRender(controlRecipes);
+  recipeView.addHandlerUpdateServings(controlServings);
+  recipeView.addHandlerBookmark(controlAddBookmark);
+  searchView.addHandlerSearch(controlSearchResult);
+  paginationView.addHandlerClick(controlPagination);
+};
+init();
