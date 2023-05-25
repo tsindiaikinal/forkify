6af75aaf9256d39aa655e1567a3f284700053845
Oleksii Tsyndiaikin <1914.ac@gmail.com>
2023-05-25 15:36:25 +0300

    [js] added new a helper function. Refactoring previous

--- src/js/helpers.js
+++ src/js/helpers.js
@@ -8,7 +8,34 @@ const timeout = function (s) {
   });
 };
 
-export const getJSON = async function (url) {
+export const trimArray = function (arr) {
+  return arr.map(el => el.trim());
+};
+
+export const AJAX = async function (url, uploadData = undefined) {
+  try {
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: JSON.stringify(uploadData),
+    };
+
+    const fetchMethod = uploadData ? fetch(url, options) : fetch(url);
+
+    const res = await Promise.race([fetchMethod, timeout(TIMEOUT_SEC)]);
+    const data = await res.json();
+
+    if (!res.ok) throw new Error(`${data.message} Error code (${res.status})`);
+    return data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+/* export const getJSON = async function (url) {
   try {
     const res = await Promise.race([fetch(url), timeout(TIMEOUT_SEC)]);
     const data = await res.json();
@@ -18,4 +45,26 @@ export const getJSON = async function (url) {
   } catch (error) {
     throw error;
   }
+}; */
+
+/* export const sendJSON = async function (url, uploadData) {
+  try {
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: JSON.stringify(uploadData),
+    };
+    const res = await Promise.race([fetch(url, options), timeout(TIMEOUT_SEC)]);
+    const data = await res.json();
+
+    if (!res.ok) throw new Error(`${data.message} Error code (${res.status})`);
+    return data;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
 };
+ */
