83048cfb744fff2ece019c0d3986ad6006545de3
Oleksii Tsyndiaikin <1914.ac@gmail.com>
2023-05-25 15:33:28 +0300

    [js] added new controllers

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,9 +1,12 @@
+import { MODAL_CLOSE_SEC } from './config';
 import * as model from './model';
+import addRecipeView from './views/addRecipeView';
 import bookmarkView from './views/bookmarkView';
 import paginationView from './views/paginationView';
 import recipeView from './views/recipeView';
 import resultView from './views/resultView';
 import searchView from './views/searchView';
+
 
 // For Parcel bilder
 /* if (module.hot) {
@@ -21,15 +24,19 @@ const controlRecipes = async function () {
     recipeView.renderSpinner();
 
     // Update result view to mark selected search result
-    resultView.render(model.getSearchResultPage());
-
+    resultView.update(model.getSearchResultPage());
+    
     // Loading recipe
     await model.loadRecipe(id);
+    
+    // Update bookmark view
+    bookmarkView.update(model.state.bookmarks);
 
     // Rendering recipe
     recipeView.render(model.state.recipe);
   } catch (err) {
     recipeView.renderError();
+    console.error(err);
   }
 };
 
@@ -80,10 +87,51 @@ const controlServings = function (newSevings) {
 };
 
 const controlAddBookmark = function () {
-  model.addBookmark(model.state.recipe);
-  
+  // Add/remove bookmark
+  if (!model.state.recipe.bookmarked) model.addBookmark(model.state.recipe);
+  else model.deleteBookmark(model.state.recipe.id);
+
+  // Update recipe view
   recipeView.update(model.state.recipe);
+
+  // Render bookmarks
+  bookmarkView.render(model.state.bookmarks);
 };
+
+const controlBookmarks = function () {
+  bookmarkView.render(model.state.bookmarks);
+};
+
+const controlAddRecipe = async function (newRecipe) {
+  // console.log(newRecipe);
+  try {
+    // Render spinner function
+    addRecipeView.renderSpinner();
+
+    await model.uploadRecipe(newRecipe);
+    console.log(model.state.recipe);
+
+    //  Success message
+    addRecipeView.renderMessage();
+
+    // Render recipe
+    recipeView.render(model.state.recipe);
+
+    // Render bookmark view
+    bookmarkView.render(model.state.bookmarks);
+
+    // Change id in url
+    window.history.pushState(null, '', `#${model.state.recipe.id}`);
+
+    // Close form popup window
+    setTimeout(() => {
+      addRecipeView.controlWindow();
+    }, MODAL_CLOSE_SEC * 1000);
+  } catch (error) {
+   console.log(error)
+    addRecipeView.renderError(error.message);
+  }
+}
 
 const init = function () {
   recipeView.addHandlerRender(controlRecipes);
@@ -91,5 +139,7 @@ const init = function () {
   recipeView.addHandlerBookmark(controlAddBookmark);
   searchView.addHandlerSearch(controlSearchResult);
   paginationView.addHandlerClick(controlPagination);
+  bookmarkView.addHandlerRender(controlBookmarks);
+  addRecipeView.addHandlerUpload(controlAddRecipe);
 };
 init();
