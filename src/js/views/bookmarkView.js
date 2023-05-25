d8a724a971ff820e95e64440fccfa76f35d73a55
Oleksii Tsyndiaikin <1914.ac@gmail.com>
2023-05-25 15:51:06 +0300

    [js] refactoring of code

--- src/js/views/bookmarkView.js
+++ src/js/views/bookmarkView.js
@@ -1,10 +1,20 @@
 import View from './View';
 import icons from '../../img/icons.svg';
+import previewView from './previewView';
 
 class BookmarkView extends View {
+  _parentElement = document.querySelector('.bookmarks__list');
+  _errorMsg = 'No bookmarks yet. Find a nice recipe and bookmark it 👨‍🍳';
+  _message = '';
 
-  constructor() {
-    super();
+  addHandlerRender(handler) {
+    window.addEventListener('load', handler);
+  }
+
+  _generateMarkup() {
+    return this._data
+      .map(bookmark => previewView.render(bookmark, false))
+      .join('');
   }
 }
 
